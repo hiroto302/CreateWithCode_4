d7bb9f61a6f6ac5c07bf1f76a727dcc191789aa4
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-22 22:51:03 +0900

    Challenge4-5 集団が出現するごとに 1 体ずつ出現数が増えるようにする

--- Assets/Challenge 4/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 4/Scripts/SpawnManagerX.cs
@@ -15,7 +15,7 @@ public class SpawnManagerX : MonoBehaviour
     public int waveCount = 1;
 
 
-    public GameObject player; 
+    public GameObject player;
 
     // Update is called once per frame
     void Update()
@@ -49,7 +49,7 @@ public class SpawnManagerX : MonoBehaviour
         }
 
         // Spawn number of enemy balls based on wave number
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < waveCount; i++)
         {
             Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
         }
