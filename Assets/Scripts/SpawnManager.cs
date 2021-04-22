c23d780ce9c78b7143d7d6ea18d1fecd91671a5d
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-22 20:26:19 +0900

    Create SingleTone SpawnManager.cs

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoSingleton<SpawnManager>
+{
+    public GameObject enemyPrefab;
+    private float _spawnRange = 9;
+
+    public void Start()
+    {
+        SpawnEnemy();
+    }
+
+    public void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), Quaternion.identity);
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-_spawnRange, _spawnRange);
+        float spawnPosZ = Random.Range(-_spawnRange, _spawnRange);
+        Vector3 spawnPos = new Vector3 (spawnPosX, 0, spawnPosZ);
+        return spawnPos;
+    }
+}
