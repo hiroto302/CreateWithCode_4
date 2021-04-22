6d4011eb75fc87a849133def1c22120fc1b947e9
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-22 20:04:17 +0900

    Create Monosingleton.cs

---
+++ Assets/Scripts/MonoSingleton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour where T: MonoSingleton<T>
+{
+    private static T _instance;
+    public static T Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                Debug.LogError(typeof(T).ToString() + " in NUll");
+                GameObject newSingletonObj = new GameObject(typeof(T).ToString());
+                newSingletonObj.AddComponent<T>();
+
+            }
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+        _instance = this as T;
+    }
+
+    public virtual void Init()
+    {
+        // optional to override
+    }
+}
