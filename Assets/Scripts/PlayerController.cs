409bb7e1e9ba31bee54738fc1204cb5a07e0ef41
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-22 21:39:21 +0900

    Player にPowerUp 機能追加

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,18 +8,50 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
     float _forwardInput;
     GameObject _focalPoint;
-    // Start is called before the first frame update
+    // has : ~した
+    public bool hasPowerup;
+    float powerupStrength = 15.0f;
+    public GameObject powerupIndicator;
+
     void Start()
     {
         _PlayerRb = GetComponent<Rigidbody>();
         _focalPoint = GameObject.Find("FocalPoint");
     }
 
-    // Update is called once per frame
     void Update()
     {
         MovePlayer();
-        Debug.Log(_focalPoint.transform.forward);
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            StartCoroutine(powerupCountdownRoutine());
+            powerupIndicator.SetActive(true);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+            enemyRb.AddForce(awayFromPlayer * powerupStrength , ForceMode.Impulse);
+        }
+    }
+
+    // Coroutines To be able to create a new loop outside of our update loop
+    IEnumerator powerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.SetActive(false);
     }
     void MovePlayer()
     {
