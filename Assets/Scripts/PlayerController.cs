ad8d260f386b0963e82cc580364b8220b7946f80
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-22 17:03:00 +0900

    Player の 移動機能

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody _PlayerRb;
+    public float speed = 5.0f;
+    float _forwardInput;
+    GameObject _focalPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _PlayerRb = GetComponent<Rigidbody>();
+        _focalPoint = GameObject.Find("FocalPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+        Debug.Log(_focalPoint.transform.forward);
+    }
+    void MovePlayer()
+    {
+        _forwardInput = Input.GetAxis("Vertical");
+        _PlayerRb.AddForce(_focalPoint.transform.forward * speed * _forwardInput);
+    }
+}
