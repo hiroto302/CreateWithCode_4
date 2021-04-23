4db2ca90bb681d472c76274bcf75b404036e6c12
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-23 11:35:44 +0900

    Update RotateCameraX.cs
    回転するオブジェクトを撮影する方法

--- Assets/Challenge 4/Scripts/RotateCameraX.cs
+++ Assets/Challenge 4/Scripts/RotateCameraX.cs
@@ -13,6 +13,7 @@ public class RotateCameraX : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         transform.Rotate(Vector3.up, horizontalInput * speed * Time.deltaTime);
 
+        // Rigidbody FreezeRotaiton x, y, z, are enable. So,The following process is required
         transform.position = player.transform.position; // Move focal point with player
 
     }
