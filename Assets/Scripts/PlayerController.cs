c2f594dcf96dc7474f36780ce2f556571fbf3840
hiroto302 <i.am.a.hiroto@gmail.com>
2021-05-31 01:30:20 +0900

    Bonus Features 4 - Smash attack
    上に飛び、下降した時に周囲の一定の範囲に衝撃の攻撃を与える機能を追加

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody _PlayerRb;
+    Rigidbody _playerRb;
     public float speed = 5.0f;
     float _forwardInput;
     GameObject _focalPoint;
@@ -17,9 +17,17 @@ public class PlayerController : MonoBehaviour
     private GameObject tmpRocket;
     private Coroutine powerupCountdown;
 
+    // Smash機能関連の変数群
+    public float hangTime = 0.5f ;          // 上昇させるのかける時間
+    public float smashSpeed = 5.0f;        // 上に飛ぶスピード
+    public float explosionForce = 50.0f;    // 敵に与える威力
+    public float explosionRadius = 10.0f;   // 範囲??
+    bool smashing = false;          // スマッシュ攻撃の最中であるか
+    float floorY;
+
     void Start()
     {
-        _PlayerRb = GetComponent<Rigidbody>();
+        _playerRb = GetComponent<Rigidbody>();
         _focalPoint = GameObject.Find("FocalPoint");
     }
 
@@ -31,7 +39,13 @@ public class PlayerController : MonoBehaviour
         // ロケットを飛ばす処理
         if (currentPowerUp == PowerUpType.Rockets && Input.GetKeyDown(KeyCode.F))
         {
-        LaunchRockets();
+            LaunchRockets();
+        }
+        // スマッシュ攻撃
+        if(currentPowerUp == PowerUpType.Smash && Input.GetKeyDown(KeyCode.Space) && !smashing)
+        {
+            smashing = true;
+            StartCoroutine(Smash());
         }
     }
 
@@ -80,7 +94,7 @@ public class PlayerController : MonoBehaviour
     void MovePlayer()
     {
         _forwardInput = Input.GetAxis("Vertical");
-        _PlayerRb.AddForce(_focalPoint.transform.forward * speed * _forwardInput);
+        _playerRb.AddForce(_focalPoint.transform.forward * speed * _forwardInput);
     }
 
     // ホーミングロケットを発射する処理
@@ -94,5 +108,39 @@ public class PlayerController : MonoBehaviour
             Quaternion.identity);
             tmpRocket.GetComponent<HomingRocket>().Fire(enemy.transform);
         }
+    }
+
+    // スマッシュ攻撃を行うための移動処理
+    IEnumerator Smash()
+    {
+        // シーン内に存在する全ての敵を取得
+        var enemies = FindObjectsOfType<Enemy>();
+        //Store the y position before taking off : 上に飛ぶ前の位置
+        floorY = transform.position.y;
+        //Calculate the amount of time we will go up : 上がるためにかける時間の量を計算
+        float jumpTime = Time.time + hangTime;
+        // 上に上昇させる処理
+        while(Time.time < jumpTime)
+        {
+            //move the player up while still keeping their x velocity : X方向の速度を維持したまま、プレイヤーを上方向に移動させる
+            _playerRb.velocity = new Vector2(_playerRb.velocity.x, smashSpeed);
+            yield return null;
+        }
+        //Now move the player down : 下に下降させる処理
+        while(transform.position.y > floorY)
+        {
+            _playerRb.velocity = new Vector2(_playerRb.velocity.x, -smashSpeed * 2);
+            yield return null;
+        }
+        //Cycle through all enemies.
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            //Apply an explosion force that originates from our position : 自分の位置を起点とした爆発力を敵に加える
+            if(enemies[i] != null)
+            enemies[i].GetComponent<Rigidbody>().AddExplosionForce(explosionForce,
+            transform.position, explosionRadius, 0.0f, ForceMode.Impulse);
+        }
+        //We are no longer smashing, so set the boolean to false : スマッシュ攻撃の終了
+        smashing = false;
     }
 }
--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // パワーアップアイテムの種類
-public enum PowerUpType { None, Pushback, Rockets }
+public enum PowerUpType { None, Pushback, Rockets, Smash }
 
 public class PowerUp : MonoBehaviour
 {
